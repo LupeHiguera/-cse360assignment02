ae83e0120cdad94e5df50ffcd37466ae77da78d1
Catgrill <Catgrill@users.noreply.github.com>
2020-10-07 20:53:48 -0700

    Initial version

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /***
+     *
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /***
+     * gets total
+     * @return returns total
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /***
+     * add a value to total
+     * @param value adds value to total
+     */
+    public void add (int value) {
+    }
+
+    /***
+     * subtracts from total
+     * @param value value subtracts from total
+     */
+    public void subtract (int value) {
+    }
+
+    /***
+     * returns message
+     * @return message
+     */
+    public String toString () {
+        return "";
+    }
+
+    /***
+     * deletes everything
+     */
+    public void clear() {
+    }
+}
